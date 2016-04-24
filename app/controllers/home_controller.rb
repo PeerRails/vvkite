946eaf1cebb1ce9b97f52ba7043c61aedfb8c9c3
Peer Rails <peer@omck.ws>
2016-04-24 22:42:13 +0300

    add specs, refactor upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,11 @@ class HomeController < ApplicationController
   def upload
     link = url_params[:link][/(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix] || nil
     status = link.match(/status\/(?<id>\d+)/)
+    @tweet = nil
     @media = nil
     unless status.nil? or status[:id].nil?
-      @media = extract_media tclient.status(status[:id])
+      @tweet = tclient.status(status[:id])
+      @media = extract_media @tweet
     end
   end
 
@@ -15,20 +17,21 @@ class HomeController < ApplicationController
     def extract_media(tweet)
       urls = []
       if tweet.media?
-        #raise tweet.media[0].video_info.variants[1].url.inspect
         tweet.media.each do |media|
           if media.class == Twitter::Media::Video or media.class == Twitter::Media::AnimatedGif
             type = media.class == Twitter::Media::Video ? "video" : "gif"
             url = {type: type}
-            url[:thumb] = media.media_url.to_s.inspect unless media.media_url.nil?
+            url[:thumb] = media.media_url_https.to_s.inspect unless media.media_url.nil?
             media.video_info.variants.map do |v|
               url[v.url.to_s.split(".").last.to_sym] = v.url.to_s
             end
             urls.push url
           elsif media.class == Twitter::Media::Photo
             url = {type: "photo"}
-            image = media.media_url.to_s
-            media.sizes.each {|s| url[s[0]] = "#{image}:#{s[0].to_s}" }
+            image = media.media_url_https.to_s
+            media.sizes.each do |s|
+              url[s[0]] = {url: "#{image}:#{s[0].to_s}", h: s[1].h, w: s[1].w}
+            end
             urls.push url
           end
         end
--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,9 @@
 tag 'vvkite-master'
+app_dir = File.expand_path("../..", __FILE__)
+port = ENV["PUMA_PORT"] || 3000
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 environment ENV.fetch("RAILS_ENV") { "development" }
-app_dir = File.expand_path("../..", __FILE__)
 pid_dir = "#{app_dir}/tmp/pids"
 pidfile "#{pid_dir}/puma.pid"
 state_path "#{pid_dir}/puma.state"
@@ -10,4 +11,4 @@ stdout_redirect nil, "#{app_dir}/log/stderr.log", true
 # preload_app!
 daemonize
 plugin :tmp_restart
-bind "tcp://0.0.0.0:#{ENV["PUMA_PORT"]}"
+bind "tcp://0.0.0.0:#{port}"
