714dd219fb397f9d978c32ad3f4b1ffd891d7612
Peer Rails <peer@omck.ws>
2016-04-24 17:31:35 +0300

    kick some functions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,40 @@ class HomeController < ApplicationController
   end
 
   def upload
+    link = url_params[:link][/(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix] || nil
+    status = link.match(/status\/(?<id>\d+)/)
+    @media = nil
+    unless status.nil? or status[:id].nil?
+      @media = extract_media tclient.status(status[:id])
+    end
   end
+
+  private
+    def extract_media(tweet)
+      urls = []
+      if tweet.media?
+        #raise tweet.media[0].video_info.variants[1].url.inspect
+        tweet.media.each do |media|
+          if media.class == Twitter::Media::Video or media.class == Twitter::Media::AnimatedGif
+            type = media.class == Twitter::Media::Video ? "video" : "gif"
+            url = {type: type}
+            url[:thumb] = media.media_url.to_s.inspect unless media.media_url.nil?
+            url[:videos] = media.video_info.variants.map{|v| v.url.to_s}
+            urls.push url
+          elsif media.class == Twitter::Media::Photo
+            url = {type: "photo"}
+            image = media.media_url.to_s
+            media.sizes.each {|s| url[s[0]] = "#{image}:#{s[0].to_s}" }
+            urls.push url
+          end
+        end
+        urls
+      else
+        nil
+      end
+    end
+
+    def url_params
+      params.permit(:link)
+    end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,13 @@
 tag 'vvkite-master'
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
-#port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
+app_dir = File.expand_path("../..", __FILE__)
+pid_dir = "#{app_dir}/tmp/pids"
+pidfile "#{pid_dir}/puma.pid"
+state_path "#{pid_dir}/puma.state"
+stdout_redirect nil, "#{app_dir}/log/stderr.log", true
 # preload_app!
+daemonize
 plugin :tmp_restart
 bind "tcp://0.0.0.0:3000"
