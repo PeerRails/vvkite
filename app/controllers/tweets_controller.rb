f92d34d85e0e55dc11cece6e0ec58f1c076b2995
Peer Rails <peer@omck.ws>
2016-06-15 22:43:56 +0300

    rm post tweet method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,15 +5,6 @@ class TweetsController < ApplicationController
     render json: tweet
   end
 
-  def upload
-    status = tclient.status(tweet_params[:link].match(/status\/(?<id>\d+)/)[:id])
-    tweet = Tweet.find_by_tweet_id(status.id)
-    if tweet.nil?
-      tweet = Tweet.add_status status
-    end
-    render json: tweet
-  end
-
   def get
     #link = url_params[:link][/(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix] || nil
     #status = link.match(/status\/(?<id>\d+)/)
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   root :to => 'home#index'
   post 'upload', :to => 'home#upload'
-  #get 'tweets/show/:tweet_id', :to => 'tweets#show'
   get 'tweets/show/:tweet_id', :to => 'tweets#show'
   get 'tweets/get', :to => 'tweets#get'
-  post 'tweets/upload', :to => 'tweets#upload'
 
 end
